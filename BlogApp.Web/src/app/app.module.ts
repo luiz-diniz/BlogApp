6722ffc57617f236e9c07de421117529a15cad2f
Luiz Diniz <luiz.gustavon@hotmail.com>
2024-03-09 15:21:15 -0300

    - AppModucle structure organization

--- BlogApp.Web/src/app/app.module.ts
+++ BlogApp.Web/src/app/app.module.ts
@@ -1,16 +1,18 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './components/app.component';
-import { PostsComponent } from './components/posts/posts.component';
-
-import { PostsService } from './services/posts.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//Components
+import { AppComponent } from './components/app.component';
+import { PostsComponent } from './components/posts/posts.component';
 import { PostComponent } from './components/post/post.component';
 import { UserComponent } from './components/user/user.component';
+
+//Services
+import { PostsService } from './services/posts.service';
 
 @NgModule({
   declarations: [
