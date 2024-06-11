9eca5e4529b8a912244ce953bf0d9ba4f7059886
Luiz Diniz <luiz.gustavon@hotmail.com>
2024-06-11 10:23:23 -0300

    - Services and Repositories objects life cycle change

--- BlogApp.Backend/BlogApp.Api/Extensions/ServicesExtensions.cs
+++ BlogApp.Backend/BlogApp.Api/Extensions/ServicesExtensions.cs
@@ -10,29 +10,29 @@ public static class ServicesExtensions
 {
     public static void AddRepositories(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<IConnectionFactory, ConnectionFactory>();
-        serviceCollection.AddSingleton<IQueryExecutor, SqlQueryExecutor>();
-        serviceCollection.AddSingleton<IUsersRepository, UsersRepository>();
-        serviceCollection.AddSingleton<IPostsRepository, PostsRepository>();
-        serviceCollection.AddSingleton<IPostsReviewsRepository, PostsReviewsRepository>();
-        serviceCollection.AddSingleton<IPostsLikesRepository, PostsLikesRepository>();
-        serviceCollection.AddSingleton<IPostsCommentsRepository, PostsCommentsRepository>();
-        serviceCollection.AddSingleton<ISavedPostsRepository, SavedPostsRepository>();
-        serviceCollection.AddSingleton<IPostsCategoriesRepository, PostsCategoriesRepository>();
+        serviceCollection.AddTransient<IConnectionFactory, ConnectionFactory>();
+        serviceCollection.AddTransient<IQueryExecutor, SqlQueryExecutor>();
+        serviceCollection.AddTransient<IUsersRepository, UsersRepository>();
+        serviceCollection.AddTransient<IPostsRepository, PostsRepository>();
+        serviceCollection.AddTransient<IPostsReviewsRepository, PostsReviewsRepository>();
+        serviceCollection.AddTransient<IPostsLikesRepository, PostsLikesRepository>();
+        serviceCollection.AddTransient<IPostsCommentsRepository, PostsCommentsRepository>();
+        serviceCollection.AddTransient<ISavedPostsRepository, SavedPostsRepository>();
+        serviceCollection.AddTransient<IPostsCategoriesRepository, PostsCategoriesRepository>();
     }
 
     public static void AddServices(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<IUsersService, UsersService>();
-        serviceCollection.AddSingleton<IPostsService, PostsService>();
-        serviceCollection.AddSingleton<IPostsReviewsService, PostsReviewsService>();
-        serviceCollection.AddSingleton<IPostsLikesService, PostsLikesService>();
-        serviceCollection.AddSingleton<IPostsCommentsService, PostsCommentsService>();
-        serviceCollection.AddSingleton<ISavedPostsService, SavedPostsService>();
-        serviceCollection.AddSingleton<IImageService, ImageService>();
-        serviceCollection.AddSingleton<IPasswordService, PasswordService>();
-        serviceCollection.AddSingleton<IAuthenticationService, AuthenticationService>();
-        serviceCollection.AddSingleton<ITokenService, TokenService>();
-        serviceCollection.AddSingleton<IPostsCategoriesService, PostsCategoriesService>();
+        serviceCollection.AddTransient<IUsersService, UsersService>();
+        serviceCollection.AddTransient<IPostsService, PostsService>();
+        serviceCollection.AddTransient<IPostsReviewsService, PostsReviewsService>();
+        serviceCollection.AddTransient<IPostsLikesService, PostsLikesService>();
+        serviceCollection.AddTransient<IPostsCommentsService, PostsCommentsService>();
+        serviceCollection.AddTransient<ISavedPostsService, SavedPostsService>();
+        serviceCollection.AddTransient<IImageService, ImageService>();
+        serviceCollection.AddTransient<IPasswordService, PasswordService>();
+        serviceCollection.AddTransient<IAuthenticationService, AuthenticationService>();
+        serviceCollection.AddTransient<ITokenService, TokenService>();
+        serviceCollection.AddTransient<IPostsCategoriesService, PostsCategoriesService>();
     }
 }
